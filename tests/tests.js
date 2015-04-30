a114a388807854970e623017b92c4a0581c89efc
Tyler Gaw <me@tylergaw.com>
2015-04-30 00:50:41 -0400

    More test setup and shots() tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,23 +1,36 @@
-console.log('Arbitrary console logging');
-// Let's test this function
-function isEven(val) {
-    return val % 2 === 0;
-}
-test('isEven()', function() {
-    ok(!isEven(0), 'Zero is an even number');
-    ok(isEven(2), 'So is two');
-    ok(isEven(-4), 'So is negative four');
-    ok(!isEven(1), 'One is not an even number');
-    ok(!isEven(-7), 'Neither is negative seven');
-});
-// Let's test this function
-function multiply(a, b) {
-    return a * b;
-}
-test('multiply()', function() {
-    ok(multiply(0, 1) === 0, 'Zero times one is zero');
-    ok(multiply(2, 2) === 4, 'Two times two is four');
-    ok(multiply(1, 3) === 3, 'One times three is three');
-    ok(multiply(10, 5) === 50, 'Five times ten is fifty');
-    ok(multiply(10, 10) === 100, 'Ten times ten is one hundred');
+var xhr = sinon.useFakeXMLHttpRequest();
+var API_URL = 'https://api.dribbble.com/v1';
+
+// We don't need a real token here because we won't be making requests to the API
+$.jribbble.setToken('1234');
+
+test('jribbble.shots urls', function(assert) {
+  var url = API_URL + '/shots';
+  assert.equal(
+    $.jribbble.shots().url, url,
+    'shots() with no arguments.'
+  );
+
+  assert.equal(
+    $.jribbble.shots({'per_page': 5, 'page': 2}).url, url + '?per_page=5&page=2',
+    'shots() with per_page and page params.'
+  );
+
+  assert.equal(
+    $.jribbble.shots('animated').url,
+    url + '?list=animated',
+    'shots() with a list name.'
+  );
+
+  assert.equal(
+    $.jribbble.shots('1234').url,
+    url + '/1234',
+    'shots() with a shot ID.'
+  );
+
+  assert.equal(
+    $.jribbble.shots('1234', {'per_page': 3}).url,
+    url + '/1234?per_page=3',
+    'shots() with a shot ID and per_page param.'
+  );
 });
