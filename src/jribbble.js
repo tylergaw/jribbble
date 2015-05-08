a2b88e164ad8ae68414185d8699cbeec2a95661d
Tyler Gaw <me@tylergaw.com>
2015-05-08 09:16:40 -0400

    Getting closer with a queue hacked in, trying to write tests for that crud

--- src/jribbble.js
+++ src/jribbble.js
@@ -18,73 +18,19 @@
     'teams'
   ];
 
-  var Queue = function() {
-    this.methods = [];
-    this.response = null;
-    this.flushed = false;
-  };
+  // Provide an object of key: value params. Get back a URL encoded string if
+  // params has keys.
+  var parseParams = function(params) {
+    var p = $.param(params);
 
-  Queue.prototype = {
-    add: function(fn) {
-      if (this.flushed) {
-        fn(this.response);
-      } else {
-        this.methods.push(fn);
-      }
-    },
-    flush: function(resp) {
-      if (this.flushed) {
-        return;
-      }
-
-      this.response = resp;
-      this.flushed = true;
-
-      while(this.methods[0]) {
-        this.methods.shift()(resp);
-      }
-    }
-  }
-
-  var jribbbleBase = {
-    get: function() {
-      if (!ACCESS_TOKEN) {
-        console.error('Jribbble: Missing Dribbble access token. Set one with $.jribbble.accessToken = YOUR_ACCESS_TOKEN. If you do not have an access token, you must register a new application at https://dribbble.com/account/applications/new');
-
-        return false;
-      }
-
-      $.ajax({
-        type: 'GET',
-        url: this.url,
-        beforeSend: function(jqxhr) {
-          jqxhr.setRequestHeader('Authorization', 'Bearer ' + ACCESS_TOKEN);
-        },
-        success: function(res) {
-          this.resolve(res);
-        }.bind(this),
-        error: function(jqxhr) {
-          this.reject(jqxhr);
-        }.bind(this)
-      });
-      // console.log(this);
-      // console.log(this.url);
-      return this;
-    },
-
-    // Provide an object of key: value params. Get back a URL encoded string if
-    // params has keys.
-    parseParams: function(params) {
-      var p = $.param(params);
-
-      if (p) {
-        return '?' + p;
-      } else {
-        return '';
-      }
+    if (p) {
+      return '?' + p;
+    } else {
+      return '';
     }
   };
 
+  // TODO: Document this function ya dingus
   var negotiateArgs = function(args) {
     if (args.length !== 0) {
       var firstArg = args[0];
@@ -110,15 +56,79 @@
     }
   };
 
-  var Shots = function(undefined, shotsOpts) {
-    $.extend(this, $.Deferred(), jribbbleBase);
+  var jribbbleBase = function() {
+    var ext = $.extend({}, $.Deferred());
 
-    this.queue = new Queue();
-    this.url = API_URL + '/shots';
-    this.initArgsArray = [].slice.call(arguments);
-    this.initOpts = shotsOpts || {};
-    this.queue.flush(this);
-    return this;
+    var Queue = function() {
+      this.methods = [];
+      this.response = null;
+      this.flushed = false;
+
+      this.add = function(fn) {
+        if (this.flushed) {
+          fn(this.scope);
+        } else {
+          this.methods.push(fn);
+        }
+      };
+
+      this.flush = function(scope) {
+        if (this.flushed) {
+          return;
+        }
+
+        this.scope = scope;
+        this.flushed = true;
+
+        while(this.methods[0]) {
+          this.methods.shift()(scope);
+        }
+
+        return scope;
+      };
+
+      return this;
+    };
+
+    ext.queue = new Queue();
+    ext.url = API_URL;
+
+    ext.get = function() {
+      if (!ACCESS_TOKEN) {
+        console.error('Jribbble: Missing Dribbble access token. Set one with $.jribbble.accessToken = YOUR_ACCESS_TOKEN. If you do not have an access token, you must register a new application at https://dribbble.com/account/applications/new');
+
+        return false;
+      }
+
+      $.ajax({
+        type: 'GET',
+        url: this.url,
+        beforeSend: function(jqxhr) {
+          jqxhr.setRequestHeader('Authorization', 'Bearer ' + ACCESS_TOKEN);
+        },
+        success: function(res) {
+          this.resolve(res);
+        }.bind(this),
+        error: function(jqxhr) {
+          this.reject(jqxhr);
+        }.bind(this)
+      });
+
+      return this;
+    }
+
+    return ext;
+  };
+
+  // var Shots = function(undefined, shotsOpts) {
+  //   $.extend(this, $.Deferred(), jribbbleBase);
+  //
+  //   this.queue = new Queue();
+  //   this.url = API_URL + '/shots';
+  //   this.initArgsArray = [].slice.call(arguments);
+  //   this.initOpts = shotsOpts || {};
+  //   this.queue.flush(this);
+  //   return this;
 
     // this.shots = function(args, opts) {
     //   var params = opts || this.initOpts;
@@ -182,35 +192,121 @@
     // };
 
     // return this;
-  };
+  // };
 
-  Shots.prototype = {
-    shots: function(args, opts) {
-      var params = opts || this.initOpts;
-      var negotiated = negotiateArgs(args || this.initArgsArray) || {};
-      var url = this.url;
+  // Shots.prototype = {
+  //   shots: function(args, opts) {
+  //     var params = opts || this.initOpts;
+  //     var negotiated = negotiateArgs(args || this.initArgsArray) || {};
+  //     var url = this.url;
+  //
+  //     if (negotiated.resource) {
+  //       url += '/' + negotiated.resource;
+  //       delete negotiated.resource;
+  //     }
+  //
+  //     url += this.parseParams($.extend(negotiated, params));
+  //
+  //     this.queue.add(function(self) {
+  //       self.url = url;
+  //     });
+  //
+  //     return this;
+  //   }
+  // }
 
-      if (negotiated.resource) {
-        url += '/' + negotiated.resource;
-        delete negotiated.resource;
-      }
+  $.jribbble.shots = function(undefined, opts) {
+    var shotsParams = opts || {};
+    var shotArgsNegotiated = negotiateArgs([].slice.call(arguments)) || {};
 
-      url += this.parseParams($.extend(negotiated, params));
+    var Shots = function() {
+      $.extend(this, jribbbleBase());
+
+      this.url += '/shots';
 
       this.queue.add(function(self) {
-        self.url = url;
+        if (shotArgsNegotiated.resource) {
+          self.url += '/' + shotArgsNegotiated.resource;
+          delete shotArgsNegotiated.resource;
+        }
+
+        self.url += parseParams($.extend(shotArgsNegotiated, shotsParams));
+      });
+
+      // Jribbble seems to need an async queue, because we need to run the
+      // server request at the end of the chain, but we will never know how
+      // long the chain is. This is a super hack way of "waiting" to make sure
+      // the queue is stocked before we flush it.
+      setTimeout(function() {
+        this.queue.flush(this).get();
+      }.bind(this), 0);
+
+      return this;
+    };
+
+    Shots.prototype.attachments = function() {
+      this.queue.add(function(self) {
+        self.url += '/attachments';
       });
 
       return this;
-    }
-  }
+    };
 
-  $.jribbble.shots = function(undefined, opts) {
-    var shots = new Shots();
-    // console.log(Shots());
+    Shots.prototype.buckets = function() {
+      this.queue.add(function(self) {
+        self.url += '/buckets';
+      });
 
-    return shots.shots(undefined, opts);
+      return this;
+    };
+
+    return new Shots();
+
+
+    // this.queue = new Queue();
+    // this.url = API_URL + '/shots';
+    //
+    // // TODO: no
+    // var self = this;
+    //
+    // var params = opts || {};
+    // var negotiated = negotiateArgs([].slice.call(arguments)) || {};
+    //
+    // if (negotiated.resource) {
+    //   this.url += '/' + negotiated.resource;
+    //   delete negotiated.resource;
+    // }
+    //
+    // this.url += parseParams($.extend(negotiated, params));
+    //
+    // // this.queue.add(function() {
+    //   // return this.url;
+    // // }.bind(this));
+    //
+    // this.queue.flush(this.url);
+    // return this;
   };
+
+  // $.jribbble.shots.prototype = {
+  //   attachments: function(undefined, opts) {
+  //     this.url += '/attachments';
+  //     var params = opts || {};
+  //     var negotiated = negotiateArgs([].slice.call(arguments)) || {};
+  //
+  //     if (negotiated.resource) {
+  //       this.url += '/' + negotiated.resource;
+  //       delete negotiated.resource;
+  //     }
+  //
+  //     this.url += parseParams($.extend(negotiated, params));
+  //
+  //     this.queue.add(function(self) {
+  //       self.url = url;
+  //     });
+  //
+  //     return this;
+  //   }
+  // };
 
   $.jribbble.setToken = function(token) {
     ACCESS_TOKEN = token;
--- tests/tests.js
+++ tests/tests.js
@@ -4,43 +4,51 @@ var API_URL = 'https://api.dribbble.com/v1';
 // We don't need a real token here because we won't be making requests to the API
 $.jribbble.setToken('1234');
 
-test('jribbble.shots urls', function(assert) {
+test('jribbble.shots', function(assert) {
   var url = API_URL + '/shots';
-  assert.equal(
-    $.jribbble.shots().url, url,
-    'shots() with no arguments.'
-  );
+
+  var shots = function(args) {
+    var shots = $.jribbble.shots(args);
+    console.log(args.per_page);
+    console.log(shots.url);
+    return shots;
+  };
+
+  // assert.equal(
+  //   shots().url, url,
+  //   'shots() with no arguments.'
+  // );
 
   assert.equal(
-    $.jribbble.shots({'per_page': 5, 'page': 2}).url, url + '?per_page=5&page=2',
+    shots({'per_page': 5, 'page': 2}).url, url + '?per_page=5&page=2',
     'shots() with per_page and page params.'
   );
-
-  assert.equal(
-    $.jribbble.shots('animated').url,
-    url + '?list=animated',
-    'shots() with a list name.'
-  );
-
-  assert.equal(
-    $.jribbble.shots('1234').url,
-    url + '/1234',
-    'shots() with a shot ID.'
-  );
-
-  assert.equal(
-    $.jribbble.shots('1234', {'per_page': 3}).url,
-    url + '/1234?per_page=3',
-    'shots() with a shot ID and per_page param.'
-  );
+  //
+  // assert.equal(
+  //   $.jribbble.shots('animated').url,
+  //   url + '?list=animated',
+  //   'shots() with a list name.'
+  // );
+  //
+  // assert.equal(
+  //   $.jribbble.shots('1234').url,
+  //   url + '/1234',
+  //   'shots() with a shot ID.'
+  // );
+  //
+  // assert.equal(
+  //   $.jribbble.shots('1234', {'per_page': 3}).url,
+  //   url + '/1234?per_page=3',
+  //   'shots() with a shot ID and per_page param.'
+  // );
 });
 
-test('jribbble.shots.attachments urls', function(assert) {
-  var url = API_URL + '/shots/1234';
-
-  assert.equal(
-    $.jribbble.shots('1234').attachments().url,
-    url + '/attachments',
-    'All attachments() for the shot 1234.'
-  );
-});
+// test('jribbble.shots.attachments urls', function(assert) {
+//   var url = API_URL + '/shots/1234';
+//
+//   assert.equal(
+//     $.jribbble.shots('1234').attachments().url,
+//     url + '/attachments',
+//     'All attachments() for the shot 1234.'
+//   );
+// });
